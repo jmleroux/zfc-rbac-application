94d727e381ee4a30c2cc4388bc2e4b1fb0f6e3c3
jmleroux <jmleroux.pro@gmail.com>
2014-06-20 10:11:29 +0200

    tests

--- tests/ApplicationTest/Controller/IndexControllerTest.php
+++ tests/ApplicationTest/Controller/IndexControllerTest.php
@@ -23,6 +23,7 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
         $this->assertModuleName('Application');
         $this->assertControllerName('Application\Controller\Index');
         $this->assertControllerClass('IndexController');
+        $this->assertActionName('index');
         $this->assertMatchedRouteName('home');
     }
 
@@ -31,5 +32,53 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
         $this->dispatch('/');
         $response = $this->getResponse();
         $this->assertContains('ZfcRbac Application on GitHub', $response->getContent());
+    }
+
+    public function testMemberOnlyActionCannotBeAccessedAnonymously()
+    {
+        $this->dispatch('/member-only');
+        $this->assertResponseStatusCode(403);
+
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertActionName('memberOnly');
+        $this->assertMatchedRouteName('member-only');
+    }
+
+    public function testAdminOnlyActionCannotBeAccessedAnonymously()
+    {
+        $this->dispatch('/admin-only');
+        $this->assertResponseStatusCode(403);
+
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertActionName('adminOnly');
+        $this->assertMatchedRouteName('admin-only');
+    }
+
+    public function testAdminOrMemberOnlyActionCannotBeAccessedAnonymously()
+    {
+        $this->dispatch('/admin-or-member');
+        $this->assertResponseStatusCode(403);
+
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertActionName('adminOrMember');
+        $this->assertMatchedRouteName('admin-or-member');
+    }
+
+    public function testOtherOnlyActionCannotBeAccessedAnonymously()
+    {
+        $this->dispatch('/other-only');
+        $this->assertResponseStatusCode(403);
+
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertActionName('otherOnly');
+        $this->assertMatchedRouteName('other-only');
     }
 }
